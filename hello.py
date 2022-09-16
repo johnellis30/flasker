69a987c5e4047ba0ab1c0b1ad028b735ebdc91fb
John Ellis <john.d.ellis01@gmail.com>
2022-09-16 07:35:31 -0400

    Add alert to name form

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -42,6 +42,7 @@ def name():
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ''
+        flash("Form Submitted Successfully")
     return render_template("name.html",
         name=name,
         form=form)
