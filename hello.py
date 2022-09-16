679934ecfd49c594fb431f6e07bfdbc0b909a726
John Ellis <john.d.ellis01@gmail.com>
2022-09-15 23:51:39 -0400

    Built name form

--- hello.py
+++ hello.py
@@ -1,8 +1,18 @@
+
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
-#Create a Flask Instance
+
+# Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my super secret key"
 
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField("Fuck Off")
 
 # Create index route decorator
 @app.route('/')
@@ -22,6 +32,20 @@ def index():
 
 def user(name):
     return render_template("user.html", name=name)
+
+# Name form page
+@app.route('/name', methods=['GET', 'POST'])
+
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html",
+        name=name,
+        form=form)
+
 
 
 # Invalid URL Error
