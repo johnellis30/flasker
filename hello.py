2a3a3f0fe1c0ede7ca07e3961b29038c68e18b40
John Ellis <john.d.ellis01@gmail.com>
2022-09-17 13:53:12 -0400

    Added static files for css js and img

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
 
 
 # Create a Flask Instance
